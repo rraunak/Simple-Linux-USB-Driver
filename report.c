2c175960c43196f3a420c3a161dae2e260a9ff36
rraunak <59102409+rraunak@users.noreply.github.com>
2020-05-06 14:52:56 -0700

    Update report.c

--- report.c
+++ report.c
@@ -38,6 +38,8 @@ static void __exit sample_exit(void)
 	usb_deregister(&sample_driver);
 }
 
+int usb_control_msg(struct usb_device *dev, unsigned int pipe, __u8 request, __u8 requesttype, __u16 value, __u16 index, void *data, __u16 size, int timeout);
+
 module_init(sample_init);
 module_exit(sample_exit);
 
