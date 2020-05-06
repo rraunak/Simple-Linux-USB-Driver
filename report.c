392caf812f629bdf0cf408d60e22995dff7a6ccc
rraunak <59102409+rraunak@users.noreply.github.com>
2020-05-06 14:57:56 -0700

    Update report.c

--- report.c
+++ report.c
@@ -40,6 +40,8 @@ static void __exit sample_exit(void)
 
 int usb_control_msg(struct usb_device *dev, unsigned int pipe, __u8 request, __u8 requesttype, __u16 value, __u16 index, void *data, __u16 size, int timeout);
 
+int usb_bulk_msg(struct usb_device *usb_dev, unsigned int pipe, void *data, int len, int *actual_length, int timeout);
+
 module_init(sample_init);
 module_exit(sample_exit);
 
