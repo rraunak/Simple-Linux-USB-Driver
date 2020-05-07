60349b3029a730cde5bd149ba006769de8c4b588
rraunak <59102409+rraunak@users.noreply.github.com>
2020-05-06 21:11:34 -0700

    Update report.c

--- report.c
+++ report.c
@@ -1,13 +1,89 @@
 #include <linux/module.h>
 #include <linux/kernel.h>
 #include <linux/usb.h>
+
+#define  Max_count  512
+#define  MIN(x,y) (((x) <= (y)) ? (x) : (y))
+#define  BULK_IN  0x02
+#define  BULK_OUT  0x81
+
+static struct usb_device *usb_dev;
+static struct usb_class_driver usb_class;
+static unsigned char data[Max_size]; 
+
+static int sample_open(struct inode *inode, struct file *file)
+{
+	return 0;
+}
+static int sample_close(struct inode *inode, struct file *file)
+{
+	return 0;
+}
+static ssize_t sample_read(struct file *file, char __user *buf, size_t count, loff_t *offset)
+{
+	int ret,ret2;
+	int read_count;
+	
+	ret = usb_bulk_msg(usb_dev, usb_rcvbulkpipe(usb_dev, BULK_IN), data, Max_size, &read_count, 10000);
+	if(ret)
+	{
+		printk("Bulk message returned:%d\n, ret");
+		return ret;
+	}
+	ret2 = copy_to_user(buf, data, MIN(count, read_count));
+	if(ret2)
+	{
+		return -EINVAL;
+	}
+	return MIN(count, read_count);
+}
+static ssize_t sample_write(struct file *file, char __user *buf, size_t count, loff_t *offset)
+{
+	int ret,ret2;
+	int write_count;
+	write_count = MIN(count, Max_count);
+	
+	ret2 = copy_to_user(data, buf, MIN(count, Max_count));
+	if(ret2)
+	{
+		return -EINVAL;
+	}
+	ret = usb_bulk_msg(usb_dev, usb_sndbulkpipe(usb_dev, BULK_OUT), data, MIN(count, Max_size), &write_count, 10000);
+	if(ret)
+	{
+		printk("Bulk message returned:%d\n, ret");
+		return ret;
+	}
+	return write_count;
+}
+
+static struct file_operations fops =
+{
+	.owner = THIS_MODULE,
+	.open = sample_open,
+	.release = sample_close,
+	.read = sample_read,
+	.write = sample_write,
+};
+	
+	
 static int sample_probe(struct usb_interface *interface, const struct usb_device_id *id)
 {
+	int ret;
+	usb_dev = interface_to_usbdev(interface);
+	usb_class.name = "usb%d";
+	usb_class.fops = &fops;
+	ret = usb_register_dev(interface, &usb_class);
+	if(ret)
+	{
+		printk("Minor number not allocated\n");
+	}
 	printk("Sample Pen-drive (%04X:%04X) plugged in\n", id->idVendor, id->idProduct);
-	return 0;
+	return ret;
 }
 static void sample_disconnect(struct usb_interface *interface)
 {
+	usb_deregister_dev(interface, &usb_class);
 	printk("Sample Pen-drive removed\n");
 }
 static struct usb_device_id sample_table[] =
@@ -37,140 +113,6 @@ static void __exit sample_exit(void)
 {
 	usb_deregister(&sample_driver);
 }
-
-int usb_control_msg(struct usb_device *dev, unsigned int pipe, __u8 request, __u8 requesttype, __u16 value, __u16 index, void *data, __u16 size, int timeout);
-
-int usb_bulk_msg(struct usb_device *usb_dev, unsigned int pipe, void *data, int len, int *actual_length, int timeout);
-
-int ret;
-ret = usb_submit_urb(urb, GFP_KERNEL);
-if(ret)
-{
-	printk(“urb submission unsuccessful\n”);
-}
-
-usb_fill_bulk_urb(urb,dev->udev,usb_sndbulkpipe(dev->udev,dev->bulk_out_endpointAddr), buf, count, write_bulk_callback, dev);
-
-copy_from_user(Buffer, user_buffer, count);
-
-Buffer = usb_buffer_alloc(dev->udev, count, GFP_KERNEL, &urb->transfer_dma);
-
-urb = usb_alloc_urb(0, GFP_KERNEL);
-
-int ret;
-ret = usb_register_dev(interface, &usb_class);
-if(ret)
-{
-	printk(“minor number not allocated\n”);
-	usb_set_interface(interface, NULL);
-}
-
-static inline void usb_set_intfdata(struct usb_interface *intf, void *data);
-
-static void __exit sample_usb_exit(void)
-{
-            usb_deregister(&sample_driver);
-}
-
-static int __init sample_usb_init(void)
-{
-	int ret;
-	ret = usb_register(&sample_driver);
-	if(ret)
-	{
-		printk(“usb_register unsuccessful, error number is:%d”,ret);
-	}
-	return ret;
-}
-
-
-static struct usb_driver sample_driver = {
-	.owner          = THIS_MODULE,
-	.name           = sample,
-	.id_table       = sample_id_table
-	.probe          = sample_probe,
-	.disconnect	= sample_disconnect,
-};
-
-const struct usb_device_id *id_table;
-
-static struct usb_device_id sample_id_table [] = {
-	{ USB_DEVICE(vendor_id, product_id) },
-	{ }
-};
-MODULE_DEVICE_TABLE(usb, id_table);
-
-void (*usb_complete_t) (
-	struct urb*,
-	struct pt_regs*
-);
-
-urb->status == 0
-
-return -EINVAL;
-
-int usb_submit_urb(struct urb *urb, int mem_flags);
-
-for (i=0; i < USBVIDEO_NUMSBUF; i++) {
-      int j, k;
-      struct urb *urb = uvd->sbuf[i].urb;
-      urb->dev = dev;
-      urb->context = uvd;
-      urb->pipe = usb_rcvisocpipe(dev, uvd->video_endp);
-      urb->interval = 1;
-      urb->transfer_flags = URB_ISO_ASAP;
-      urb->transfer_buffer = uvd->sbuf[i].data;
-      urb->complete = usbvideo_IsocIrq;
-      urb->number_of_packets = FRAMES_PER_DESC;
-      urb->transfer_buffer_length = uvd->iso_packet_len * FRAMES_PER_DESC;
-      for (j=k=0; j < FRAMES_PER_DESC; j++, k += uvd->iso_packet_len) {
-          urb->iso_frame_desc[j].offset = k;
-          urb->iso_frame_desc[j].length = uvd->iso_packet_len;
-      }
-  }
-
-	
-unsigned char *setup_packet
-
- void usb_fill_int_urb (
-	struct urb *urb,
-	struct usb_device *dev,
-	unsigned int pipe,
-	unsigned char *setup_packet,
-	void *transfer_buffer,
-	int buffer_length,
-	usb_complete_t complete,
-	void *context,	
-	);
-
- void usb_fill_int_urb (
-	struct urb *urb,
-	struct usb_device *dev,
-	unsigned int pipe,
-	void *transfer_buffer,
-	int buffer_length,
-	usb_complete_t complete,
-	void *context,	
-	);
-
- void usb_fill_int_urb (
-	struct urb *urb,
-	struct usb_device *dev,
-	unsigned int pipe,
-	void *transfer_buffer,
-	int buffer_length,
-	usb_complete_t complete,
-	void *context,
-	int interval	
-	);
-
-void usb_free_urb(struct urb *urb)
-
-struct urb *usb_alloc_urb(int iso_packets, int mem_flags);	
-
-enum usb_device_state state;
-
-atomic_t pm_usage_cnt;
 	
 module_init(sample_init);
 module_exit(sample_exit);
